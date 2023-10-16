8316008ae89ce3c6668069276b866df629831a27
AylinOrjuela <acorjuelal@udistrital.edu.co>
2023-10-16 11:21:34 -0500

    Diseño de modulo finca

---
+++ app/src/main/java/com/jpdev/proyectogrado/homePageFincaActivity.kt
@@ -0,0 +1,11 @@
+package com.jpdev.proyectogrado
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class homePageFincaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page_finca)
+    }
+}
