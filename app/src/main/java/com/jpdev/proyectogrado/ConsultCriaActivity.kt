37949a5d8bf720c93e7d041f411e470450e4adba
JuanPCFdev <jpcuellarf@udistrital.edu.co>
2023-10-22 18:58:47 -0500

    pagina principal ganado, consultar cira y levante

---
+++ app/src/main/java/com/jpdev/proyectogrado/ConsultCriaActivity.kt
@@ -0,0 +1,11 @@
+package com.jpdev.proyectogrado
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConsultCriaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_consult_cria)
+    }
+}
