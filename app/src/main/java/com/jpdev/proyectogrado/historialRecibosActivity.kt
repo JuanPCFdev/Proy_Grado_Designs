04586e85ecfbe655b339ec65e88a5ad41ed1e84d
AylinOrjuela <acorjuelal@udistrital.edu.co>
2023-10-27 17:33:24 -0500

    Se agregaron vistas de finanzas

---
+++ app/src/main/java/com/jpdev/proyectogrado/historialRecibosActivity.kt
@@ -0,0 +1,11 @@
+package com.jpdev.proyectogrado
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class historialRecibosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_historial_recibos)
+    }
+}
