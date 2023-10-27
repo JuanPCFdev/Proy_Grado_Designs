f85426ee31ecc2b71d5ee136d768e2b7a5bb5a70
JuanPCFdev <jpcuellarf@udistrital.edu.co>
2023-10-26 19:17:19 -0500

    Adicion de editar ganado cria

---
+++ app/src/main/java/com/jpdev/proyectogrado/EditGanadoCriaActivity.kt
@@ -0,0 +1,11 @@
+package com.jpdev.proyectogrado
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditGanadoCriaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_ganado_cria)
+    }
+}
