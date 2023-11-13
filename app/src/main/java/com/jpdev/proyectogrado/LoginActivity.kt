3e44e94ffbacf0ee927d661affb518c745c51c3f
JuanPCFdev <jpcuellarf@udistrital.edu.co>
2023-11-13 16:29:38 -0500

    initialize binding and logic for LogIn

--- app/src/main/java/com/jpdev/proyectogrado/LoginActivity.kt
+++ app/src/main/java/com/jpdev/proyectogrado/LoginActivity.kt
@@ -1,11 +1,49 @@
 package com.jpdev.proyectogrado
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jpdev.proyectogrado.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityLoginBinding
+    private var credentials = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if(binding.etUser.text.toString() != ""
+            && binding.etUser.text.toString().isNotEmpty()
+            && binding.etPassword.text.toString() != ""
+            &&binding.etPassword.text.toString().isNotEmpty()){
+            //User ID and Password is not empty -> Validate data base
+            initListeners()//Initializing listeners
+        }
+
+
     }
+
+    private fun initListeners(){
+        binding.btnLogin.setOnClickListener {
+            //Logic to login
+            validateCredentials()//Verify if credentials match with inputs
+        }
+        binding.btnRegister.setOnClickListener {
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent) //Go to Activity Register
+        }
+        binding.btnPassRecord.setOnClickListener {
+            //Record Password
+        }
+    }
+
+    private fun validateCredentials(){
+        if(credentials){
+            //Log In to app
+        }else{
+            //Toast(this,"Credenciales no validas",Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
